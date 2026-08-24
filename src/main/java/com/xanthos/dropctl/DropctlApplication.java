8350db1beb91858799e3b7833977b6005839caa1
BeingShashwat <shashwatm878@gmail.com>
2026-08-25 03:12:10 +0530

    starting project

---
+++ src/main/java/com/xanthos/dropctl/DropctlApplication.java
@@ -0,0 +1,13 @@
+package com.xanthos.dropctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropctlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropctlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xanthos/dropctl/DropctlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanthos.dropctl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropctlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
